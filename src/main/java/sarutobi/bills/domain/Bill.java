f148b378ca73a5222c08515b1b03467811f5cb31
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2017-11-12 15:30:44 +0400

    Описание одного счета

--- src/main/java/sarutobi/bills/domain/Bill.java
+++ src/main/java/sarutobi/bills/domain/Bill.java
@@ -3,16 +3,64 @@ package sarutobi.bills.domain;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.YearMonth;
-import java.util.List;
 
 /**
+ *  Выставленный счет.
  * Created by sarutobi.
  */
-public class Bill {
-    private String name;
-    private YearMonth billPeriod;
-    private LocalDate billDate;
-    private LocalDate paidDate;
-    private List<BillRow> rowNames;
-    private BigDecimal fine;
+public final class Bill {
+    private int id;
+    private String name; // Наименование счета
+    private YearMonth billPeriod; // Период выставления счета
+    private LocalDate billDate; // Дата выписки счета
+    private LocalDate paidDate; // Дата оплаты счета
+    private BigDecimal fine; // Пени по счету на дату выписки.
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public YearMonth getBillPeriod() {
+        return billPeriod;
+    }
+
+    public void setBillPeriod(YearMonth billPeriod) {
+        this.billPeriod = billPeriod;
+    }
+
+    public LocalDate getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(LocalDate billDate) {
+        this.billDate = billDate;
+    }
+
+    public LocalDate getPaidDate() {
+        return paidDate;
+    }
+
+    public void setPaidDate(LocalDate paidDate) {
+        this.paidDate = paidDate;
+    }
+
+    public BigDecimal getFine() {
+        return fine;
+    }
+
+    public void setFine(BigDecimal fine) {
+        this.fine = fine;
+    }
 }
--- src/main/java/sarutobi/bills/domain/BillRow.java
+++ src/main/java/sarutobi/bills/domain/BillRow.java
@@ -4,13 +4,81 @@ import java.math.BigDecimal;
 import java.time.YearMonth;
 
 /**
+ *  Строка данных по счету.
  * Project: bills
  * Created by sarutobi at 12.11.17.
  */
 public final class BillRow {
-    private String itemName;
-    private double itemsCount;
-    private BigDecimal itemPrice;
-    private BigDecimal totalPrice;
-    private YearMonth forPeriod;
+    private int id;
+    private YearMonth forPeriod; // Расчетный период
+    private String itemName;  // Наименование
+    private BigDecimal itemPrice; // Тариф
+    private double itemsCount; // Потребление
+    private BigDecimal countPrice; // Сумма за потребление
+    private BigDecimal recalculation; // Перерасчеты
+    private BigDecimal totalPrice; // Итого к оплате
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public YearMonth getForPeriod() {
+        return forPeriod;
+    }
+
+    public void setForPeriod(YearMonth forPeriod) {
+        this.forPeriod = forPeriod;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public BigDecimal getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(BigDecimal itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public double getItemsCount() {
+        return itemsCount;
+    }
+
+    public void setItemsCount(double itemsCount) {
+        this.itemsCount = itemsCount;
+    }
+
+    public BigDecimal getCountPrice() {
+        return countPrice;
+    }
+
+    public void setCountPrice(BigDecimal countPrice) {
+        this.countPrice = countPrice;
+    }
+
+    public BigDecimal getRecalculation() {
+        return recalculation;
+    }
+
+    public void setRecalculation(BigDecimal recalculation) {
+        this.recalculation = recalculation;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
